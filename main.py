6c06e3b5eac147d1f5038846a4e0c2094c9c5f55
linuxfight <72513124+linuxfight@users.noreply.github.com>
2022-10-02 00:38:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import asyncio
 import httpx
 import json
 import uuid
+
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultCachedSticker, InlineQuery, CallbackQuery, Message
 
@@ -65,45 +67,76 @@ background_color = config('background_color')
 url = config('url')
 
 
+def convert_entities(entities):
+    result = []
+    for entity in entities:
+        entity_name = str(entity.type.name).lower()
+        converted_entity = {
+            "offset": entity.offset,
+            "length": entity.length,
+            "type": entity_name
+        }
+        if entity_name == "custom_emoji":
+            converted_entity["custom_emoji_id"] = entity.custom_emoji_id
+        result.append(converted_entity)
+    return result
+
+
 def convert_message(message: Message, value: bool):
     if value is False:
         if not message.sticker and message.caption:
             message.text = get_text(message, value)
     result = {
-        'from': {
+        'from': get_from(message),
+        'text': message.text,
+        'entities': None,
+        'reply_to_message': None,
+        'media': get_media(message)
+    }
+    if message.entities:
+        result['entities'] = convert_entities(message.entities)
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        reply.text = get_text(message.reply_to_message, True)
+        result['reply_to_message'] = convert_message(reply, True)
+    return result
+
+
+def get_from(message: Message):
+    result = None
+    if message.from_user:
+        result = {
             'id': message.from_user.id,
             'first_name': message.from_user.first_name,
             'last_name': message.from_user.last_name,
             'username': message.from_user.username
-        },
-        'text': message.text,
-        'reply_to_message': None,
-        'media': get_media(message)
-    }
+        }
+    if message.forward_from_chat:
+        result = {
+            'id': message.forward_from_chat.id,
+            'name': message.forward_from_chat.title,
+            'username': message.forward_from_chat.username
+        }
     if message.forward_from or message.forward_sender_name or message.forward_from_chat:
         if message.forward_from_chat:
-            result['from'] = {
+            result = {
                 'id': message.forward_from_chat.id,
                 'name': message.forward_from_chat.title,
                 'username': message.forward_from_chat.username
             }
         elif message.forward_sender_name:
-            result['from'] = {
+            result = {
                 'id': message.forward_sender_name.__hash__(),
                 'name': message.forward_sender_name,
                 'username': "HiddenSender"
             }
         else:
-            result['from'] = {
+            result = {
                 'id': message.forward_from.id,
                 'first_name': message.forward_from.first_name,
                 'last_name': message.forward_from.last_name,
                 'username': message.forward_from.username
             }
-    if message.reply_to_message:
-        reply = message.reply_to_message
-        reply.text = get_text(message.reply_to_message, True)
-        result['reply_to_message'] = convert_message(reply, True)
     return result
 
 
@@ -196,7 +229,7 @@ def get_reaction(user_id, quote):
 
 def get_args(text: str):
     args = {
-        'color': config('color'),
+        'color': config('background_color'),
         'messages': 1,
         'reply': False,
         'png': False,
@@ -206,27 +239,67 @@ def get_args(text: str):
     data = text.split()
     for argument in data:
         if argument.isnumeric():
-            if 1 <= int(argument) <= 5:
-                args['messages'] = argument
+            if 1 < int(argument) < 6:
+                args['messages'] = int(argument)
         if argument == "reply":
             args['reply'] = True
-        if argument == "img":
-            args['img'] = True
-        if argument == "png":
-            args['img'] = False
-            args['png'] = True
         if argument == "rate":
             args['rate'] = True
+        if argument == "png":
+            args['png'] = True
+        if argument == "img":
+            args['img'] = True
     return args
 
 
-async def create_quote(message: Message):
+async def send_request(data, method):
+    async with httpx.AsyncClient() as client:
+        response = await client.request(
+            url=url,
+            json=data,
+            method=method
+        )
+        while response.status_code == 429:
+            await asyncio.sleep(int(response.headers['retry-after']))
+            response = await client.request(
+                url=url,
+                json=data,
+                method=method
+            )
+        return response
+
+
+async def create_quote(message: Message, args):
+    if not message.reply_to_message:
+        await app.send_message(
+            chat_id=message.chat.id,
+            text="Команду необходимо писать в ответ на сообщение",
+            reply_to_message_id=message.id
+        )
+        return
     request_object = {
         'bot_token': config('bot_token'),
         'emoji_library': emoji_library,
         'background_color': background_color,
         'messages': [convert_message(message.reply_to_message, False)]
     }
+    if args['reply']:
+        request_object['messages'] = [
+            convert_message(message.reply_to_message.reply_to_message, False),
+            convert_message(message.reply_to_message, False)
+        ]
+    if args['messages'] != 1:
+        num = message.reply_to_message.id
+        messages = []
+        while num != message.reply_to_message.id - args['messages']:
+            messages.append(num)
+            num -= 1
+        result = await app.get_messages(chat_id=message.reply_to_message.chat.id, message_ids=messages, replies=-1)
+        request_object['messages'] = []
+        num = args['messages']
+        while num != 0:
+            request_object['messages'].append(convert_message(result[num - 1], False))
+            num -= 1
     quote = {
         'Id': storage['nextQuoteId'],
         'text': get_text(message.reply_to_message, False),
@@ -234,11 +307,7 @@ async def create_quote(message: Message):
         'likes': [],
         'dislikes': []
     }
-    async with httpx.AsyncClient() as client:
-        response = await client.post(url=url, json=request_object)
-        while response.status_code == 429:
-            await asyncio.sleep(int(response.headers['retry-after']))
-            response = await client.post(url=url, json=request_object)
+    response = await send_request(data=request_object, method="POST")
     storage['nextQuoteId'] += 1
     storage['Quotes'].append(quote)
     with open("quote.webp", 'wb') as quote_file:
@@ -256,7 +325,7 @@ async def create_quote(message: Message):
 
 @app.on_message(filters.command(["q"]))
 async def message_handler(client, message: Message):
-    await create_quote(message=message)
+    await create_quote(message=message, args=get_args(message.text))
 
 
 def get_quote(quote_id):
