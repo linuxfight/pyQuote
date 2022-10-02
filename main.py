8f80660b2de404b59bbe39d9826e909a85554585
linuxfight <72513124+linuxfight@users.noreply.github.com>
2022-10-03 00:19:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQue
 
 
 def config(key):
-    if exists("config.txt"):
-        data = json.loads(open("config.txt", 'r').read())
+    if exists("config.json"):
+        data = json.loads(open("config.json", 'r').read())
         return str(data[key])
     else:
         data = {
@@ -25,7 +25,7 @@ def config(key):
             'api_id': 'API_ID',
             'url': "https://quotes.vanutp.dev/generate"
         }
-        open("config.txt", 'w').write(json.dumps(data))
+        open("config.json", 'w').write(json.dumps(data))
         quit(0)
 
 
