fecd105d0138dca621a99812bc4e00c86ea6bd82
linuxfight <72513124+linuxfight@users.noreply.github.com>
2022-10-18 22:04:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,62 @@
 import asyncio
-import os
-import json
-import re
-import yt_dlp
+import httpx
 import aiofiles
+import json
+import uuid
+import tempfile
+
 
 from os.path import exists
+from pathlib import Path
 from pyrogram import Client, filters, enums
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultCachedSticker, InlineQuery, CallbackQuery, Message
 
 
 def config(key):
-    if exists("config.txt"):
-        data = json.loads(open("config.txt", 'r').read())
+    if exists("config.json"):
+        data = json.loads(open("config.json", 'r').read())
         return str(data[key])
     else:
         data = {
+            'emoji_library': 'twemoji',
+            'background_color': '#1b1429',
             'bot_token': 'BOT_TOKEN',
             'api_hash': 'API_HASH',
-            'api_id': 'API_ID'
+            'api_id': 'API_ID',
+            'url': "https://quotes.vanutp.dev/generate"
         }
-        open("config.txt", 'w').write(json.dumps(data))
+        open("config.json", 'w').write(json.dumps(data))
         quit(0)
 
 
 def load():
     if exists("save.json"):
-        with open("save.json", 'r') as file:
-            data = file.read()
+        data = open("save.json", 'r').read()
         return json.loads(data)
     else:
-        data = {}
-        with open("save.json", 'w') as file:
-            file.write(json.dumps(data))
+        data = {
+            'Quotes': [],
+            'nextQuoteId': 0
+        }
+        open("save.json", 'w').write(json.dumps(data))
         return load()
 
 
 async def save(self):
+    data = {
+        'Quotes': self['Quotes'],
+        'nextQuoteId': self['nextQuoteId']
+    }
     async with aiofiles.open("save.json", 'w') as file:
-        await file.write(json.dumps(self))
+        await file.write(json.dumps(data))
+    quote_id = data['nextQuoteId'] - 1
 
 
 def login():
-    if exists("tgdl_bot.session"):
-        return Client("tgdl_bot")
+    if exists("pyQuote_bot.session"):
+        return Client("pyQuote_bot")
     return Client(
-        "tgdl_bot",
+        "pyQuote_bot",
         api_id=config('api_id'),
         api_hash=config('api_hash'),
         bot_token=config('bot_token')
@@ -54,204 +65,349 @@ def login():
 
 app = login()
 storage = load()
+emoji_library = config('emoji_library')
+background_color = config('background_color')
+url = config('url')
 
 
-def get_video_id(url: str):
-    regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.'
-                       r'(com|be)/(watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})')
-
-    match = regex.match(url)
-
-    if not match:
-        return None
-    return match.group('id')
-
-
-def is_url(url):
-    match_obj = re.match(r'^((?:https?:)?//)?((?:www|m)\.)?(youtube(-nocookie)?\.com|youtu.be)(/(?:[\w\-]+\?v=|embed/|v'
-                         r'/)?)([\w\-]+)(\S+)?$', url)
-    if match_obj:
-        return True
-    return False
+def convert_entities(entities):
+    result = []
+    for entity in entities:
+        entity_name = str(entity.type.name).lower()
+        converted_entity = {
+            "offset": entity.offset,
+            "length": entity.length,
+            "type": entity_name
+        }
+        if entity_name == "custom_emoji":
+            converted_entity["custom_emoji_id"] = str(entity.custom_emoji_id)
+        result.append(converted_entity)
+    return result
 
 
-def generate_keyboard(video_id):
+def convert_message(message: Message, value: bool):
+    if value is False:
+        if not message.sticker and message.caption:
+            message.text = get_text(message, value)
+    result = {
+        'from': get_from(message),
+        'text': message.text,
+        'entities': None,
+        'reply_to_message': None,
+        'media': get_media(message)
+    }
+    if message.entities:
+        result['entities'] = convert_entities(message.entities)
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        reply.text = get_text(message.reply_to_message, True)
+        result['reply_to_message'] = convert_message(reply, True)
+    return result
+
+
+def get_from(message: Message):
+    result = None
+    if message.from_user:
+        result = {
+            'id': message.from_user.id,
+            'first_name': message.from_user.first_name,
+            'last_name': message.from_user.last_name,
+            'username': message.from_user.username
+        }
+    if message.forward_from_chat:
+        result = {
+            'id': message.forward_from_chat.id,
+            'name': message.forward_from_chat.title,
+            'username': message.forward_from_chat.username
+        }
+    if message.forward_from or message.forward_sender_name or message.forward_from_chat:
+        if message.forward_from_chat:
+            result = {
+                'id': message.forward_from_chat.id,
+                'name': message.forward_from_chat.title,
+                'username': message.forward_from_chat.username
+            }
+        elif message.forward_sender_name:
+            result = {
+                'id': message.forward_sender_name.__hash__(),
+                'name': message.forward_sender_name,
+                'username': "HiddenSender"
+            }
+        else:
+            result = {
+                'id': message.forward_from.id,
+                'first_name': message.forward_from.first_name,
+                'last_name': message.forward_from.last_name,
+                'username': message.forward_from.username
+            }
+    return result
+
+
+def get_media(message: Message):
+    media = None
+    if message.photo:
+        media = {
+            'file_id': message.photo.file_id,
+            'type': "photo"
+        }
+    if message.sticker:
+        media = {
+            'file_id': message.sticker.file_id,
+            'type': "sticker"
+        }
+    return media
+
+
+def get_text(message: Message, is_reply: bool):
+    msg = message.text
+
+    if message.photo:
+        if message.caption:
+            if is_reply:
+                msg = "🖼️ " + message.caption
+            else:
+                msg = message.caption
+        else:
+            msg = "🖼️"
+
+    if message.sticker:
+        if message.sticker.emoji:
+            if is_reply:
+                msg = "Стикер " + message.sticker.emoji
+            else:
+                msg = message.sticker.emoji
+        else:
+            msg = "Стикер"
+
+    return msg
+
+
+def generate_keyboard(quote):
     buttons = [[
         InlineKeyboardButton(
-            text="⬇ Скачать видео 🎥",
-            callback_data=video_id + ":video"
+            text="👍" + str(len(quote['likes'])),
+            callback_data=str(quote['Id']) + ":like"
         ),
         InlineKeyboardButton(
-            text="⬇ Скачать аудио 🎙",
-            callback_data=video_id + ":audio"
+            text="👎" + str(len(quote['dislikes'])),
+            callback_data=str(quote['Id']) + ":dislike"
         )]]
     return InlineKeyboardMarkup(
         buttons
     )
 
 
-async def download(video_url, download_type):
-    video_id = get_video_id(video_url)
-    output_filename = video_id + '.mkv'
-    command = f'yt-dlp --merge-output-format mkv -o "{output_filename}" {video_url} --quiet'
+def set_like(user_id, quote):
+    if user_id in quote['dislikes']:
+        quote['dislikes'].remove(user_id)
 
-    if download_type == "audio":
-        output_filename = video_id + '.mp3'
-        command = f'yt-dlp -f "ba" -x --audio-format mp3 -o "{output_filename}" {video_url} --quiet'
+    if user_id not in quote['likes']:
+        quote['likes'].append(user_id)
 
-    process = await asyncio.create_subprocess_shell(
-        command
-    )
 
-    await process.communicate()
+def set_dislike(user_id, quote):
+    if user_id in quote['likes']:
+        quote['likes'].remove(user_id)
 
-    while process.returncode != 0:
-        pass
+    if user_id not in quote['dislikes']:
+        quote['dislikes'].append(user_id)
+
+
+def remove_reactions(user_id, quote):
+    if user_id in quote['likes']:
+        quote['likes'].remove(user_id)
+
+    if user_id in quote['dislikes']:
+        quote['dislikes'].remove(user_id)
+
+
+def get_reaction(user_id, quote):
+    if user_id in quote['likes']:
+        return "like"
+    elif user_id in quote['dislikes']:
+        return "dislike"
     else:
-        while not exists(output_filename):
-            pass
-        else:
-            return output_filename
+        return "none"
 
 
-@app.on_message(filters.command("start"))
-async def start_message(client, message: Message):
-    await app.send_message(
-        chat_id=message.chat.id,
-        text="Привет, я помогу тебе скачать видео с Youtube! Отправь мне ссылку и я отправлю тебе видео.",
-        reply_to_message_id=message.id
-    )
+def get_args(text: str):
+    args = {
+        'color': config('background_color'),
+        'messages': 1,
+        'reply': False,
+        'png': False,
+        'img': False,
+        'rate': False
+    }
+    data = text.split()
+    for argument in data:
+        if argument.isnumeric():
+            if 1 < int(argument) < 6:
+                args['messages'] = int(argument)
+        if argument == "reply":
+            args['reply'] = True
+        if argument == "rate":
+            args['rate'] = True
+        if argument == "png":
+            args['png'] = True
+        if argument == "img":
+            args['img'] = True
+    return args
 
 
-@app.on_message()
-async def on_link(client, msg: Message):
-    text = None
-    try:
-        text = msg.text.split()
-    except:
-        pass
-    url = None
-    try:
-        for t in text:
-            if is_url(t):
-                url = t
-    except:
-        pass
-    if not url:
+async def send_request(data, method):
+    async with httpx.AsyncClient() as client:
+        response = await client.request(
+            url=url,
+            json=data,
+            method=method
+        )
+        while response.status_code == 429:
+            await asyncio.sleep(int(response.headers['retry-after']))
+            response = await client.request(
+                url=url,
+                json=data,
+                method=method
+            )
+        return response
+
+
+async def create_quote(message: Message, args):
+    if not message.reply_to_message:
+        await app.send_message(
+            chat_id=message.chat.id,
+            text="Команду необходимо писать в ответ на сообщение",
+            reply_to_message_id=message.id
+        )
         return
+    request_object = {
+        'bot_token': config('bot_token'),
+        'emoji_library': emoji_library,
+        'background_color': background_color,
+        'messages': [convert_message(message.reply_to_message, False)]
+    }
+    if args['reply']:
+        request_object['messages'] = [
+            convert_message(message.reply_to_message.reply_to_message, False),
+            convert_message(message.reply_to_message, False)
+        ]
+    if args['messages'] != 1:
+        num = message.reply_to_message.id
+        messages = []
+        while num != message.reply_to_message.id - args['messages']:
+            messages.append(num)
+            num -= 1
+        result = await app.get_messages(chat_id=message.reply_to_message.chat.id, message_ids=messages, replies=-1)
+        request_object['messages'] = []
+        num = args['messages']
+        while num != 0:
+            request_object['messages'].append(convert_message(result[num - 1], False))
+            num -= 1
+    quote = {
+        'Id': storage['nextQuoteId'],
+        'text': get_text(message.reply_to_message, False),
+        'fileId': "",
+        'likes': [],
+        'dislikes': []
+    }
+    quote_id = storage['nextQuoteId']
+    response = await send_request(data=request_object, method="POST")
+    with tempfile.TemporaryDirectory() as tmp:
+        filename = Path(tmp) / 'quote.webp'
 
-    with yt_dlp.YoutubeDL() as ydl:
-        info: dict = ydl.extract_info(url, download=False)
-    title = info.get('title', None)
-    description = info.get('description', None)
+        async with aiofiles.open(filename, 'wb') as quote_file:
+            await quote_file.write(response.content)
 
-    await app.send_message(
-        chat_id=msg.chat.id,
-        reply_to_message_id=msg.id,
-        reply_markup=generate_keyboard(get_video_id(url)),
-        text=f"Название: {title}\n"
-             f"Ссылка: {url}\n"
-             f"Описание: \n{description}"
-    )
+            await app.send_chat_action(
+                chat_id=message.chat.id,
+                action=enums.ChatAction.CHOOSE_STICKER
+            )
+
+            sent_message = await app.send_sticker(
+                chat_id=message.chat.id,
+                sticker=str(filename),
+                reply_to_message_id=message.id,
+                reply_markup=generate_keyboard(quote)
+            )
+    quote['fileId'] = sent_message.sticker.file_id
+    storage['nextQuoteId'] += 1
+    storage['Quotes'].append(quote)
+    await save(storage)
+
+
+@app.on_message(filters.command(["q"]))
+async def message_handler(client, message: Message):
+    await create_quote(message=message, args=get_args(message.text))
+
+
+def get_quote(quote_id):
+    for quoteFor in storage['Quotes']:
+        if str(quoteFor['Id']) == quote_id:
+            return quoteFor
 
 
 @app.on_callback_query()
-async def on_callback_query(client, callback_query: CallbackQuery):
+async def on_button_click(client, callback_query: CallbackQuery):
     data = callback_query.data.split(':')
-    video_id = data[0]
-    download_type = data[1]
-    with yt_dlp.YoutubeDL() as ydl:
-        info: dict = ydl.extract_info(
-            download=False,
-            url=f'https://youtu.be/{video_id}'
+    user_id = callback_query.from_user.id
+    quote_id = data[0]
+    reaction = data[1]
+    quote = get_quote(quote_id)
+
+    if reaction == get_reaction(user_id, quote):
+        remove_reactions(user_id, quote)
+        await app.answer_callback_query(
+            callback_query_id=callback_query.id,
+            text="Вы убрали реакцию"
         )
-    filename = video_id + '.mkv'
-    telegram_filename = info['title'] + '.mkv'
-    if download_type == 'audio':
-        filename = video_id + '.mp3'
-        telegram_filename = info['title'] + '.mp3'
-
-    if f'{video_id}_{download_type}' in storage:
-        if storage[f'{video_id}_{download_type}'] != None:
-            if storage[f'{video_id}_{download_type}'] != "Working":
-                file = storage[f'{video_id}_{download_type}']
-                if download_type == 'video':
-                    await app.send_chat_action(
-                        chat_id=callback_query.message.chat.id,
-                        action=enums.ChatAction.UPLOAD_VIDEO
-                    )
-                    await app.send_document(
-                        chat_id=callback_query.message.chat.id,
-                        reply_to_message_id=callback_query.message.id,
-                        document=file,
-                        file_name=telegram_filename
-                    )
-                else:
-                    await app.send_chat_action(
-                        chat_id=callback_query.message.chat.id,
-                        action=enums.ChatAction.UPLOAD_AUDIO
-                    )
-                    await app.send_audio(
-                        chat_id=callback_query.message.chat.id,
-                        reply_to_message_id=callback_query.message.id,
-                        audio=file,
-                        file_name=telegram_filename
-                    )
-                return
-
-    try:
-        if f'{video_id}_{download_type}' in storage and storage[f'{video_id}_{download_type}'] == "Working":
-            await callback_query.answer(
-                text="⚙ Производится скачивание, пожалуйста подождите"
-            )
-            return
-
-        await callback_query.answer(
-            text="⚙ Обработка запроса"
+    elif reaction == "like":
+        set_like(user_id, quote)
+        await app.answer_callback_query(
+            callback_query_id=callback_query.id,
+            text="Вы поставили лайк"
         )
-    except:
-        return
+    elif reaction == "dislike":
+        set_dislike(user_id, quote)
+        await app.answer_callback_query(
+            callback_query_id=callback_query.id,
+            text="Вы поставили дизлайк"
+        )
 
-    storage.update({f'{video_id}_{download_type}': "Working"})
-
-    file_path = await download(f'https://youtu.be/{video_id}', download_type)
-    await app.send_chat_action(
-        chat_id=callback_query.message.chat.id,
-        action=enums.ChatAction.UPLOAD_DOCUMENT
-    )
-    file = None
-    if download_type == 'video':
-        await app.send_chat_action(
+    if callback_query.message:
+        await app.edit_message_reply_markup(
             chat_id=callback_query.message.chat.id,
-            action=enums.ChatAction.UPLOAD_VIDEO
-        )
-        file = await app.send_document(
-            chat_id=callback_query.message.chat.id,
-            reply_to_message_id=callback_query.message.id,
-            document=file_path,
-            file_name=telegram_filename
+            message_id=callback_query.message.id,
+            reply_markup=generate_keyboard(quote)
         )
     else:
-        await app.send_chat_action(
-            chat_id=callback_query.message.chat.id,
-            action=enums.ChatAction.UPLOAD_AUDIO
+        await callback_query.edit_message_reply_markup(
+            reply_markup=generate_keyboard(quote)
         )
-        file = await app.send_audio(
-            chat_id=callback_query.message.chat.id,
-            reply_to_message_id=callback_query.message.id,
-            audio=file_path,
-            file_name=telegram_filename
-        )
-    file_id = None
-    if file.document:
-        file_id = file.document.file_id
-    elif file.video:
-        file_id = file.video.file_id
-    else:
-        file_id = file.audio.file_id
-    storage.update({f'{video_id}_{download_type}': file_id})
-    os.remove(file_path)
+
     await save(storage)
+
+
+@app.on_inline_query()
+async def on_inline_query(client, inline_query: InlineQuery):
+    quotes = []
+    user_id = inline_query.from_user.id
+    for quote in storage['Quotes']:
+        if (user_id in quote['likes'] or user_id in quote['dislikes']) and inline_query.query is not None and\
+                quote['text'] is not None and inline_query.query.lower() in quote['text'].lower():
+            quotes.append(quote)
+
+    answers = []
+
+    for quote in quotes:
+        random_id = str(uuid.uuid4())
+        answers.append(InlineQueryResultCachedSticker(id=random_id, sticker_file_id=quote['fileId'],
+                                                      reply_markup=generate_keyboard(quote)))
+
+    await app.answer_inline_query(
+        inline_query_id=inline_query.id,
+        results=answers,
+        cache_time=0
+    )
 
 
 if __name__ == '__main__':
